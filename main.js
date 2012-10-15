df50776726b7c5e700d079edf9a1025f9e5a63da
sjacke <sean.jacke@learnship.de>
2012-10-15 12:29:37 +0200

    Renamed classes to components and added main.js

---
+++ main.js
@@ -0,0 +1,13 @@
+require.config({
+    baseUrl: '/js/components',
+    paths: {
+        jquery: '/js/libs/jquery/1.7.1/jquery',
+        underscore: '/js/libs/underscore/1.3.1-amdjs/underscore',
+        backbone: '/js/libs/backbone/0.9.1-amdjs/backbone',
+        interfaces: '/js/interfaces',
+        mixins: '/js/mixins',
+        plugins: '/js/plugins',
+        collections: '/js/collections',
+        models: '/js/models',
+        templates: '/js/templates' 
+});
